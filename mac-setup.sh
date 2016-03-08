646277cef07b1b2fcba9002028c8dac30c02ec7b
manthasriram <sriram.mantha@gmail.com>
2016-03-07 22:37:28 -0600

    Update mac-setup.sh

--- mac-setup.sh
+++ mac-setup.sh
@@ -2,11 +2,15 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 #install git bash
+echo "installing git bash completion script...."
 brew install bash-completion
 
 #install cask to install java
+echo "installing java...."
 brew tap caskroom/cask
 brew install brew-cask
 brew cask install java
 
-#
+#maven
+echo "installing maven...."
+brew install maven
